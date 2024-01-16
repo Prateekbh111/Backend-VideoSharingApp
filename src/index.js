f469c6582aae484627242ccecb97592aea3d5968
Prateek Bhardwaj <prateekbh111@gmail.com>
2024-01-16 19:38:31 +0530

    connect to mongoAtlas

---
+++ src/index.js
@@ -0,0 +1,49 @@
+//data base is always in another continent
+
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+
+
+dotenv.config({path: './env'})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express'
+const app = express();
+
+
+//professional start this lines with semicolon
+;( async () => {
+    try
+    {
+        await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}');
+        app.on("error", (error)=> {
+            console.error("Error: ", error);
+            throw err;
+        })
+
+        app.listen(process.env.PORT, ()=> {
+            console.log(`Server running on port ${process.env.PORT}`);
+        })
+    }
+    catch(error)
+    {
+        console.error("Error: ", error);
+        throw err;
+    }
+})();
+*/
